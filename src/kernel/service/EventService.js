e2b9922de9e6481e1c427effa323d1231cdc4f9a
Zhilin Liu <zhilin7@qq.com>
2023-07-30 19:48:08 +0800

    feat: handle composition event

--- src/kernel/service/EventService.js
+++ src/kernel/service/EventService.js
@@ -4,9 +4,13 @@ import { handleInput } from '../utils/handleInput'
 export class EventService {
   constructor(kernel) {
     this._kernel = kernel
+    this._isComposing = false
   }
 
   _onSelectionChange() {
+    if (this._isComposing) {
+      return
+    }
     const range = document.getSelection().getRangeAt(0)
     const kernelRange = this._kernel.toKernelRange(range)
     this._kernel.setKernelRange(kernelRange)
@@ -15,13 +19,31 @@ export class EventService {
 
   _onBeforeInput(event) {
     event.preventDefault()
+    if (this._isComposing) {
+      return
+    }
     const { inputType, data } = event
     handleInput(inputType, data, this._kernel)
+  }
+
+  _onCompositionStart() {
+    this._isComposing = true
+  }
+
+  _onCompositionEnd(event) {
+    this._isComposing = false
+    const data = event.data
+    const range = window.getSelection().getRangeAt(0)
+    range.setStart(range.startContainer, range.startOffset - data.length)
+    range.deleteContents()
+    handleInput('insertText', data, this._kernel)
   }
 
   mount() {
     const rootElement = this._kernel.rootElement
     document.addEventListener('selectionchange', this._onSelectionChange.bind(this))
     rootElement.addEventListener('beforeinput', this._onBeforeInput.bind(this))
+    rootElement.addEventListener('compositionstart', this._onCompositionStart.bind(this))
+    rootElement.addEventListener('compositionend', this._onCompositionEnd.bind(this))
   }
 }
