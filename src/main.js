f2cf87735ca516d131ed0b1e57f2ff1c2d81c0e8
Zhilin Liu <zhilin7@qq.com>
2023-08-06 13:39:49 +0800

    chore: install package instead of cdn

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './style.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
 const app = createApp(App)
